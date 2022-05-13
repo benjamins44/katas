b18890dfa953f699b8110102dab785a2c198d362
bcorre <benjamin.corre-ext@nickel.eu>
2022-05-13 17:16:29 +0200

    init

---
+++ src/test/bco/stringcalculator/TotoTest.java
@@ -0,0 +1,12 @@
+package bco.stringcalculator;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TotoTest {
+  @Test
+  void test() {
+    Assertions.assertThat(true).isFalse();
+  }
+}
